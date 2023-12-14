2f407fb882aab54bf80f83f3a0f00a1559e3348e
Aleksandr Myroniuk <miron.alikc@gmail.com>
2023-12-14 04:24:07 -0800

    Set Users Data Screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,10 @@ import 'package:driver_app/feature/auth/bloc/auth_bloc.dart';
 import 'package:driver_app/feature/auth/repositoey/auth_repository.dart';
 import 'package:driver_app/feature/set_users_data/bloc/set_users_data_bloc.dart';
 import 'package:driver_app/feature/set_users_data/repositories/set_users_data_repositories.dart';
+import 'package:driver_app/feature/set_users_data/set_users_data_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-import 'feature/auth/auth_screen.dart';
 import 'feature/registration/bloc/registration_bloc.dart';
 import 'feature/registration/repositories/registration_repository.dart';
 
@@ -31,7 +30,7 @@ class MyApp extends StatelessWidget {
               dio: RepositoryProvider.of<DioSettings>(context).dio),
         ),
         RepositoryProvider(
-          create: (context) => AuthRepository(
+          create: (context) => SetUsersDataRepository(
               dio: RepositoryProvider.of<DioSettings>(context).dio),
         ),
       ],
@@ -44,14 +43,14 @@ class MyApp extends StatelessWidget {
             ),
           ),
           BlocProvider(
-            create: (context) => AuthBloc(
-              repository: RepositoryProvider.of<AuthRepository>(context),
-            ),
-          ),
-          BlocProvider(
             create: (context) => SetUsersDataBloc(
               repository:
                   RepositoryProvider.of<SetUsersDataRepository>(context),
+            ),
+          ),
+          BlocProvider(
+            create: (context) => AuthBloc(
+              repository: RepositoryProvider.of<AuthRepository>(context),
             ),
           ),
         ],
@@ -59,10 +58,10 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
             textTheme: GoogleFonts.ibmPlexMonoTextTheme(),
           ),
-          home: const AuthScreen(),
+         home: const SetUsersDataScreen(),
           //  home: const SetUsersDataScreen(),
           // home: const SplashScreen(),
-          // home: HomePage(),
+        //  home: HomePage(),
         ),
       ),
     );
